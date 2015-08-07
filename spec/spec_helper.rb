f981d6263769592cbd466fdc3ace6881d0fefa53
Miroslav Hettes <mirrec@gmail.com>
2015-08-07 11:08:20 +0200

    Ensure support helpers are loaded in alphabet order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each {|f| require f}
 
 # This is here because the vcr library saves its cassettes in yaml format and psych throws segfaults if the string being saved has an invalid utf-8 character in it.
 # Can be removed when the issue is resolved https://github.com/myronmarston/vcr/issues/74
