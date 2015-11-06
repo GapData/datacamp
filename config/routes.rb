a36aea30fbebc52e0f24b0b385484b48c10dc2a7
Tomas Kramar <kramar.tomas@gmail.com>
2015-11-06 13:14:11 +0100

    Add english how to

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,10 @@ Datacamp::Application.routes.draw do
   match '/register' => 'sessions#create'
   match '/signup' => 'sessions#new'
 
-  match '/how-to' => 'main#howto', :as => :howto
-
   scope "(:locale)", :locale => /sk|en/ do
 
     match '/pages/api-access' => 'main#api_access'
+    match '/how-to' => 'main#howto', :as => :howto
 
     resources :errors
 
